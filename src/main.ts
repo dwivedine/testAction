6a6bb19f0804ca3c4f9282e9082d761a6dec6ae6
Neha Dwivedi <dwivedine@github.com>
2021-05-07 16:23:53 +0530

    Printing environment variables value

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as idaction from '@dwivedine/oidcclientpublic'
+//import * as idaction from '../../../oidcClientPublic/oidcClientPublic/lib/main'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const body = idaction.getIDToken(
+      'ghactions',
+      'HelloWorld123#',
+      'https://ghactionsoidc.azurewebsites.net/connect/token/'
+    )
+    core.debug(`body is ${body}`)
   } catch (error) {
     core.setFailed(error.message)
   }
